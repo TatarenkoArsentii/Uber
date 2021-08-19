99bb7fba9f4a18e4338700b9379edd89b123adec
Arsentii <tatarenkoarsen@gmail.com>
2021-08-19 14:29:28 +0300

    added header

---
+++ js/script.js
@@ -0,0 +1,6 @@
+function disc (){
+let promo = parseInt((document.getElementById('discount').value).match(/\d+/), 10);
+// let discount = parseInt(promo.match(/\d+/), 10);
+console.log(promo);
+// console.log(discount);
+}
